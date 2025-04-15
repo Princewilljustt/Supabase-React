29ec991a59c775ee96389c7e73937ce781bac4f7
prince <princewillifeanyi15@gmail.com>
2025-04-15 23:29:52 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState,useEffect } from 'react'
+import supabase from "./supabase-client"
+
+
+const App = () => {
+
+  const [users, setUsers] = useState([])
+  // console.log(users)
+
+  useEffect(() => {
+   fetchUsers()
+    }, [])
+  
+
+  async function fetchUsers() {
+    const {data} = await supabase
+    .from('users')
+    .select('*')
+    setUsers(data)
+    console.log(users)
+
+  }
+  return (
+    <div>
+      
+      
+      App
+      </div>
+  )
+}
+
+export default App
---
+++ src/config/supabaseClient.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+
+ export const supabase = createClient(
+    "https://aoxbaiithkhcauyeptqi.supabase.co",
+     `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFveGJhaWl0aGtoY2F1eWVwdHFpIiwicm9
+sZSI6ImFub24iLCJpYXQiOjE3MzM0OTgwOTEsImV4cCI6MjA0OTA3NDA5MX0.lTWcqTGTVyireEyIN0Wnvg2a2_PBp3QEMPoL5zaeZ2w`
+)
+
